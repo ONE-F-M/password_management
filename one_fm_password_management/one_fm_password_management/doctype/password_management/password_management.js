6a0df032c906b977dfb17c8f7bb6cfc64e45e16d
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-04-02 12:18:41 +0530

    feat: Password Managment - Validate url and evaluate password Strength

--- one_fm_password_management/one_fm_password_management/doctype/password_management/password_management.js
+++ one_fm_password_management/one_fm_password_management/doctype/password_management/password_management.js
@@ -3,6 +3,35 @@
 
 frappe.ui.form.on('Password Management', {
 	password: function(frm) {
-    console.log(frm.doc.password);
+    frappe.call({
+      doc: frm.doc,
+      method: 'check_my_password_strength',
+      callback: function(r) {
+        frm.set_value('password_strength', r.message?r.message:'');
+      }
+    });
+	},
+  password_strength: function(frm) {
+    let strength_list = {'Week': 'red', 'Good': 'orange', 'Strong': 'green'};
+    let description = '';
+    if(frm.doc.password_strength){
+      description = '<font color="'+strength_list[frm.doc.password_strength]+'">'+__(frm.doc.password_strength)+'</font>';
+    }
+    frm.set_df_property('password', 'description', description);
+  },
+  url: function(frm) {
+    frappe.call({
+      doc: frm.doc,
+      method: 'validate_my_url',
+      callback: function(r) {
+        if(r.message){
+          frm.set_value("valid_url", true);
+        }
+        if(!r.message && frm.doc.url){
+          frm.set_value("valid_url", false);
+          frappe.throw(__("The Given Url is not Valid."));
+        }
+      }
+    });
 	}
 });
--- one_fm_password_management/one_fm_password_management/doctype/password_management/password_management.py
+++ one_fm_password_management/one_fm_password_management/doctype/password_management/password_management.py
@@ -3,8 +3,49 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe, re
 from frappe.model.document import Document
 
 class PasswordManagement(Document):
-	pass
+	def validate(self):
+		if self.url:
+			if not validate_url(self.url):
+				frappe.throw(_("The Given Url is not Valid."))
+			else:
+				self.valid_url = True
+
+	def check_my_password_strength(self):
+		if self.password:
+			return check_password_strength(self.password)
+		return False
+
+	def validate_my_url(self):
+		if self.url:
+			return validate_url(self.url)
+		return False
+
+@frappe.whitelist()
+def check_password_strength(pwd):
+	# ref: https://www.codespeedy.com/check-the-password-strength-in-python/
+	if(len(pwd)>=8):
+		# must contain one digit similarly we say that for lowercase, uppercase and special characters
+		if(bool(re.match('((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,30})', pwd))==True):
+			return 'Strong'
+		elif(bool(re.match('((\d*)([a-z]*)([A-Z]*)([!@#$%^&*]*).{8,30})', pwd))==True):
+			return 'Good'
+	else:
+		return 'Week'
+
+@frappe.whitelist()
+def validate_url(url):
+	regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+	if(bool(re.match(regex, url))==True):
+		return True
+	return False
