3c0b31b022d9e4baa8d4649a3e5bf1819a6e28f0
Jamsheer PP <kuttypjamsheer@gmail.com>
2020-04-07 11:20:39 +0530

    fix: Menu for Password Management

---
+++ one_fm_password_management/config/tools.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Tools"),
+			"icon": "octicon octicon-briefcase",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Password Management",
+					"label": _("Password Management"),
+					"description": _("Password Management")
+				}
+			]
+		}
+	]
